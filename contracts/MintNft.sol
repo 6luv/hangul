17c8dab1830841cbe83d99ae847e9939c97611b6
6luv <jmj00005@naver.com>
2024-06-14 13:33:41 +0900

    refactor: mint contract

--- contracts/MintNft.sol
+++ contracts/MintNft.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.26;
 
 import { ERC1155 } from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 contract MintNft is ERC1155 {
     string public name;
@@ -14,7 +15,22 @@ contract MintNft is ERC1155 {
         metadataUri = _metadataUri;
     }
 
-    function mintNft() public {
-        
+    function mintNft(uint _tokenId, uint _amount) public {
+        require(_tokenId > 0 && _tokenId <= 38, "No exist token.");
+        _mint(msg.sender, _tokenId, _amount, "");
+    }
+
+    function uri(uint _tokenId) public view override returns (string memory) {
+        return string(abi.encodePacked(metadataUri, Strings.toString(_tokenId), ".json"));
+    }
+
+    function balanceOfNfts(address _owner) public view returns (uint[38] memory) {
+        uint[38] memory result;
+
+        for (uint i = 0; i < 38; i ++) {
+            result[i] = balanceOf(_owner, i + 1);
+        }
+
+        return result;
     }
 }
