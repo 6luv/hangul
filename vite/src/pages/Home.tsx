b196b6084f67bcc19cd07c6cef53571df2efb20b
6luv <jmj00005@naver.com>
2024-06-18 14:36:40 +0900

    style: home style

--- vite/src/pages/Home.tsx
+++ vite/src/pages/Home.tsx
@@ -15,119 +15,100 @@ import { BiStore } from "react-icons/bi";
 import { IoExtensionPuzzleOutline } from "react-icons/io5";
 import { FaRegUser } from "react-icons/fa";
 
+const HomeMenu = [
+  {
+    title: "환영합니다!",
+    description: `한글 맞춤법 문제를 통해 NFT를 얻을 수 있는 공간입니다.
+    맞춤법 문제를 풀고 한글 자모 NFT를 발행해보세요.
+    발행한 NFT는 가게에서 거래할 수 있으며,
+    창작 탭에서는 보유한 NFT를 직접 조합하여 자신만의 단어를 만들 수 있습니다.
+    함께 한글을 더욱 아름답게 만들어보세요!`,
+    icon: (
+      <Text fontSize={40} fontWeight="bold">
+        한글
+      </Text>
+    ),
+  },
+  {
+    title: "문제",
+    description: "한글 맞춤법 문제를 풀고 NFT 발행 기회를 얻어보세요.",
+    icon: <HiOutlineClipboardList size={"80px"} />,
+  },
+  {
+    title: "발행",
+    description: "한글 자모로 이루어진 NFT를 획득할 수 있습니다.",
+    icon: <RiCoinLine size="80px" />,
+  },
+  {
+    title: "가게",
+    description: "내가 소유한 NFT를 거래할 수 있습니다.",
+    icon: <BiStore size="80px" />,
+  },
+  {
+    title: "창작",
+    description: "내가 소유한 NFT로 자신만의 독특한 단어를 만들 수 있습니다.",
+    icon: <IoExtensionPuzzleOutline size="80px" />,
+  },
+  {
+    title: "내 페이지",
+    description: "내가 소유한 NFT를 확인하고 판매 등록할 수 있습니다.",
+    icon: <FaRegUser size="80px" />,
+  },
+];
+
 const Home: FC = () => {
   return (
-    <Flex flexDir="column" w="100%">
+    <Flex flexDir="column" w="100%" minH="50vh">
       <Flex
         className="mainFlexStyle"
         flexDir="column"
-        justifyContent="center"
+        justifyContent="space-between"
         alignItems="center"
       >
-        <Flex h="70%" flexDir="column" alignItems="center">
-          <Text
-            fontWeight="bold"
-            fontSize={[28, 32, 56]}
-            textAlign="center"
-            w="full"
-          >
-            한글
-          </Text>
-          <Tabs mt={4}>
-            <TabList gap={4}>
-              <Tab>
-                <Flex
-                  flexDir="column"
-                  alignItems="center"
-                  p={2}
-                  rounded="lg"
-                  boxShadow="0 4px 6px darkgray"
-                >
-                  <HiOutlineClipboardList size={"80px"} />
-                </Flex>
-              </Tab>
-              <Tab>
-                <Flex
-                  flexDir="column"
-                  alignItems="center"
-                  p={2}
-                  rounded="lg"
-                  boxShadow="0 4px 6px darkgray"
-                >
-                  <RiCoinLine size="80px" />
-                </Flex>
-              </Tab>
-              <Tab>
-                <Flex
-                  flexDir="column"
-                  alignItems="center"
-                  p={2}
-                  rounded="lg"
-                  boxShadow="0 4px 6px darkgray"
-                >
-                  <BiStore size="80px" />
-                </Flex>
-              </Tab>
-              <Tab>
-                <Flex
-                  flexDir="column"
-                  alignItems="center"
-                  p={2}
-                  rounded="lg"
-                  boxShadow="0 4px 6px darkgray"
-                >
-                  <IoExtensionPuzzleOutline size="80px" />
-                </Flex>
-              </Tab>
-              <Tab>
-                <Flex
-                  flexDir="column"
-                  alignItems="center"
-                  p={2}
-                  rounded="lg"
-                  boxShadow="0 4px 6px darkgray"
-                >
-                  <FaRegUser size="80px" />
-                </Flex>
-              </Tab>
+        <Tabs mt={12} size="md" isFitted variant="enclosed" colorScheme="white">
+          <Flex>
+            <TabList gap={2}>
+              {HomeMenu.map((v, i) => (
+                <Tab key={i}>
+                  <Flex
+                    flexDir="column"
+                    alignItems="center"
+                    px={3}
+                    py={4}
+                    rounded="lg"
+                    boxShadow="0 4px 6px darkgray"
+                  >
+                    {v.icon}
+                  </Flex>
+                </Tab>
+              ))}
             </TabList>
-
+          </Flex>
+          <Flex>
             <TabPanels>
-              <TabPanel>
-                <Text fontSize={[16, 20, 28]} fontWeight="bold">
-                  문제
-                </Text>
-                <Text fontSize={[12, 16, 20]} fontWeight="semibold">
-                  한글 맞춤법 문제를 풀고 NFT 발행 기회를 얻어보세요.
-                </Text>
-              </TabPanel>
-              <TabPanel>
-                <Text fontSize={[16, 20, 20]}>발행</Text>
-                <Text fontSize={[12, 16, 20]}>
-                  한글 자모로 이루어진 NFT를 획득할 수 있습니다.
-                </Text>
-              </TabPanel>
-              <TabPanel>
-                <Text fontSize={[16, 20, 20]}>가게</Text>
-                <Text fontSize={[12, 16, 20]}>
-                  내가 소유한 NFT를 거래할 수 있습니다.
-                </Text>
-              </TabPanel>
-              <TabPanel>
-                <Text fontSize={[16, 20, 20]}>창작</Text>
-                <Text fontSize={[12, 16, 20]}>
-                  내가 소유한 NFT로 자신만의 독특한 단어를 만들 수 있습니다.
-                </Text>
-              </TabPanel>
-              <TabPanel>
-                <Text fontSize={[16, 20, 20]}>내 페이지</Text>
-                <Text fontSize={[12, 16, 20]}>
-                  내가 소유한 NFT를 확인하고 판매 등록할 수 있습니다.
-                </Text>
-              </TabPanel>
+              {HomeMenu.map((v, i) => (
+                <TabPanel key={i} mt={16} border="2px" rounded="2xl">
+                  <Text
+                    fontSize={[16, 20, 24]}
+                    fontWeight="bold"
+                    borderBottom="2px"
+                  >
+                    {v.title}
+                  </Text>
+                  <Text
+                    className="description"
+                    fontSize={[12, 16, 20]}
+                    fontWeight="semibold"
+                    textAlign="center"
+                    pt={4}
+                  >
+                    {v.description}
+                  </Text>
+                </TabPanel>
+              ))}
             </TabPanels>
-          </Tabs>
-        </Flex>
+          </Flex>
+        </Tabs>
         <SlideNfts />
       </Flex>
     </Flex>
